2206d4d36cab554f7f86b973ed6bd26419d34792
aaronlow1220 <117196390+aaronlow1220@users.noreply.github.com>
2024-07-23 15:03:34 +0800

    test: createAccountTest
    - created test for create account without parent_id param

--- tests/unit/createAccountTest.php
+++ tests/unit/createAccountTest.php
@@ -81,6 +81,59 @@ class CreateAccountTest extends Unit
         $this->assertEquals($expected, $result->toArray());
     }
 
+    public function testCreateWithoutParentId(){
+        $params = [
+            'name' => '現金及約當現金fb',
+            'en_name' => 'Cash and Cash Equivsdfalentsfbqwe',
+            'is_debit' => '1',
+            'type' => '流動資產',
+            'note' => '統治大項科目',
+            'for_statement' => '0',
+            'is_need_purchase_order' => '0',
+        ];
+
+        $expected = [
+            'name' => '現金及約當現金fb',
+            'en_name' => 'Cash and Cash Equivsdfalentsfbqwe',
+            'parent_id' => 0,
+            'count' => 0,
+            'level' => 1,
+            'is_debit' => '1',
+            'type' => '流動資產',
+            'note' => '統治大項科目',
+            'for_statement' => '0',
+            'is_need_purchase_order' => '0',
+        ];
+
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->expects($this->once())->method('commit');
+        $transaction->expects($this->never())->method('rollBack');
+
+        $this->accountRepo->method('getDb')
+            ->willReturn(Stub::makeEmpty(Connection::class, ['beginTransaction' => $transaction]));
+
+        $this->accountRepo->expects($this->never())
+            ->method('update');
+
+        $this->accountRepo->expects($this->once())
+            ->method('create')
+            ->with($this->callback(function ($accountParams) use ($params) {
+                return $accountParams['name'] === $params['name']
+                    && 0 === $accountParams['count']
+                    && 1 === $accountParams['level'];
+            }))
+            ->willReturnCallback(function () use ($expected) {
+                $accountMock = $this->createMock(ActiveRecord::class);
+                $accountMock->method('toArray')->willReturn($expected);
+
+                return $accountMock;
+            });
+
+        $result = $this->service->create($params);
+
+        $this->assertEquals($expected, $result->toArray());
+    }
+
     protected function _before()
     {
         $this->accountRepo = $this->createMock(AccountRepo::class);
